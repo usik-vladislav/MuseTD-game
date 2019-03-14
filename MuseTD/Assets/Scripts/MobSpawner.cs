a8f99d49a08bcd1cc359592f9a5111c4ca9fb89f
usik-vladislav <usik.vladislav.s@gmail.com>
2019-03-14 19:18:14 +0300

    The project was created. It is the start. There are early versions of: mobs, a spawn, money, a path and songmanager

---
+++ MuseTD/Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+
+    private bool isStarted;
+
+    private Ball ball;
+
+    private void Awake()
+    {
+        isStarted = true;
+        ball = Resources.Load<Ball>("Ball");
+    }
+
+    void Update()
+    {
+        if (isStarted & SongManager.IsBeatFull)
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+        }
+    }
+}
