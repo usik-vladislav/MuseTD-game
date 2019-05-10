9921f44664e2e936bb0dee006babc8c79563025a
usik-vladislav <usik.vladislav.s@gmail.com>
2019-05-10 04:02:44 +0300

    Добавлено главное меню, без настроек, переход сразу к уровню

---
+++ MuseTD/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
